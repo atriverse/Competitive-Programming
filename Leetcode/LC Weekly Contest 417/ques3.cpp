6326f7872f62c914d36b788ba95deba728dd7efd
Abhay Trivedi <abhaytrivedi838@gmail.com>
2024-10-04 23:23:02 +0530

    Upsolved problem 3 and 4 of LC-W-C-417

--- Leetcode/LC Weekly Contest 417/ques3.cpp
+++ Leetcode/LC Weekly Contest 417/ques3.cpp
@@ -1,11 +1,11 @@
 /*
 You are given a string word and a non-negative integer k.
 
-Return the total number of 
+Return the total number of
 substrings
  of word that contain every vowel ('a', 'e', 'i', 'o', and 'u') at least once and exactly k consonants.
 
- 
+
 
 Example 1:
 
@@ -40,7 +40,7 @@ The substrings with every vowel and one consonant are:
 word[0..5], which is "ieaouq".
 word[6..11], which is "qieaou".
 word[7..12], which is "ieaouq".
- 
+
 
 Constraints:
 
@@ -53,7 +53,8 @@ word consists only of lowercase English letters.
 using namespace std;
 
 // TLE
-class Solution {
+class Solution
+{
 public:
     /**
      * Counts the number of substrings in the given word that contain all vowels and at most k consonants.
@@ -62,45 +63,144 @@ public:
      * @param k The maximum number of consonants allowed in the substrings.
      * @return The count of valid substrings.
      */
-    int countOfSubstrings(string word, int k) {
+    int countOfSubstrings(string word, int k)
+    {
         int n = word.length();
         int result = 0;
         unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
-        
-        for (int i = 0; i < n; ++i) {
+
+        for (int i = 0; i < n; ++i)
+        {
             unordered_map<char, int> vowel_count;
             int consonant_count = 0;
-            
-            for (int j = i; j < n; ++j) {
-                if (vowels.count(word[j])) {
+
+            for (int j = i; j < n; ++j)
+            {
+                if (vowels.count(word[j]))
+                {
                     vowel_count[word[j]]++;
-                } else {
-                    consonant_count++;
-                    if (consonant_count > k) break;
                 }
-                
-                if (consonant_count == k && vowel_count.size() == 5) {
+                else
+                {
+                    consonant_count++;
+                    if (consonant_count > k)
+                        break;
+                }
+
+                if (consonant_count == k && vowel_count.size() == 5)
+                {
                     bool all_vowels_present = true;
-                    for (char v : vowels) {
-                        if (vowel_count[v] == 0) {
+                    for (char v : vowels)
+                    {
+                        if (vowel_count[v] == 0)
+                        {
                             all_vowels_present = false;
                             break;
                         }
                     }
-                    if (all_vowels_present) result++;
+                    if (all_vowels_present)
+                        result++;
                 }
             }
         }
-        
+
         return result;
     }
 };
 
-int main(){
+// Optimized solution
+/*
+Intuition
+The problem naturally lends itself to a sliding window approach because we're dealing with substrings, which are contiguous segments of the original string. However, this problem requires some modifications to the general sliding window technique:
+    -> We need to keep track of two conditions simultaneously: the presence of all vowels and the count of consonants.
+    -> The valid window can extend beyond the point where we find a valid substring, as adding more characters to the right might create more valid substrings. The right can extend before the point where the next consonent found(this will alter the condition of k consonents).
+    -> The efficiency of this solution hinges on a crucial insight: we don't need to explicitly extend our window to the right to count all valid substrings starting at the left pointer. Instead, we use precalculation to achieve this in constant time.
+*/
+class Solution1
+{
+public:
+    bool isVowel(char ch)
+    {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+
+    long long countOfSubstrings(string word, int k)
+    {
+        int n = word.size();
+        unordered_map<char, int> vowels;
+        int consonantCount = 0;
+        long long result = 0;
+
+        // Precompute next consonant positions
+        vector<int> nextConsonant(n);
+        int lastConsonant = n;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            nextConsonant[i] = lastConsonant;
+            if (!isVowel(word[i]))
+                lastConsonant = i;
+        }
+
+        // Sliding window
+        int left = 0, right = 0;
+        while (right < n)
+        {
+            // Expand window
+            if (isVowel(word[right]))
+            {
+                vowels[word[right]]++;
+            }
+            else
+            {
+                consonantCount++;
+            }
+
+            // Shrink window if too many consonants
+            while (left <= right && consonantCount > k)
+            {
+                if (isVowel(word[left]))
+                {
+                    if (--vowels[word[left]] == 0)
+                        vowels.erase(word[left]);
+                }
+                else
+                {
+                    consonantCount--;
+                }
+                left++;
+            }
+
+            // Count valid substrings
+            while (left < right && vowels.size() == 5 && consonantCount == k)
+            {
+                result += (nextConsonant[right] - right);
+                if (isVowel(word[left]))
+                {
+                    if (--vowels[word[left]] == 0)
+                        vowels.erase(word[left]);
+                }
+                else
+                {
+                    consonantCount--;
+                }
+                left++;
+            }
+
+            right++;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
     string word;
     int k;
-    cin>>word>>k;
+    cin >> word >> k;
     Solution sol;
-    cout<<sol.countOfSubstrings(word,k);
+    cout << sol.countOfSubstrings(word, k) << endl;
+    Solution1 sol1;
+    cout << sol1.countOfSubstrings(word, k) << endl;
     return 0;
 }
--- Leetcode/LC Weekly Contest 417/ques4.cpp
+++ Leetcode/LC Weekly Contest 417/ques4.cpp
@@ -11,7 +11,7 @@ Return the value of the kth character in word after performing all the operation
 
 Note that the character 'z' can be changed to 'a' in the second type of operation.
 
- 
+
 
 Example 1:
 
@@ -40,7 +40,7 @@ Appends "a" to "a", word becomes "aa".
 Appends "bb" to "aa", word becomes "aabb".
 Appends "aabb" to "aabb", word becomes "aabbaabb".
 Appends "bbccbbcc" to "aabbaabb", word becomes "aabbaabbbbccbbcc".
- 
+
 
 Constraints:
 
@@ -52,8 +52,9 @@ The input is generated such that word has at least k characters after all operat
 #include <bits/stdc++.h>
 using namespace std;
 
-//TLE
-class Solution {
+// TLE
+class Solution
+{
 public:
     /**
      * Finds the kth character in the final string after performing the given operations.
@@ -62,32 +63,81 @@ public:
      * @param operations A vector of operations to perform on the initial string "a".
      * @return The kth character in the final string.
      */
-    char kthCharacter(long long k, vector<int>& operations) {
+    char kthCharacter(long long k, vector<int> &operations)
+    {
         string word = "a";
-        
-        for (int op : operations) {
-            if (op == 0) {
+
+        for (int op : operations)
+        {
+            if (op == 0)
+            {
                 word += word;
-            } else if (op == 1) {
+            }
+            else if (op == 1)
+            {
                 string newPart = "";
-                for (char c : word) {
+                for (char c : word)
+                {
                     newPart += (c == 'z') ? 'a' : (char)(c + 1);
                 }
                 word += newPart;
             }
-            
-            if (word.length() >= k) {
+
+            if (word.length() >= k)
+            {
                 break;
             }
         }
-        
+
         return word[k - 1];
     }
 };
 
-int main(){
+
+//Optimized solution
+class Solution1
+{
+public:
+    /**
+     * Finds the kth character in the final string after performing the given operations.
+     *
+     * This recursive function takes the index of the character to return (1-indexed) and the vector of operations to perform on the initial string "a". It determines the type of the current operation (0 or 1) and calculates the next index to check based on the length of the current string. It then recursively calls itself with the updated index and returns the appropriate character.
+     *
+     * @param k The index of the character to return, 1-indexed.
+     * @param op A vector of operations to perform on the initial string "a".
+     * @return The kth character in the final string.
+     */
+    char kthCharacter(long long k, vector<int> &op)
+    {
+        if (k == 1)
+            return 'a';
+        int n = op.size(), type;
+        long long len = 1, nextK;
+        for (int i = 0; i < n; i++)
+        {
+            len *= 2;
+            if (len >= k)
+            {
+                type = op[i];
+                nextK = k - len / 2;
+                break;
+            }
+        }
+        char res = kthCharacter(nextK, op);
+        if (type == 0)
+            return res;
+        if (res == 'z')
+            return 'a';
+        return res + 1;
+    }
+};
+int main()
+{
     Solution s;
     vector<int> operations = {0, 0, 0};
     cout << s.kthCharacter(5, operations) << endl;
+
+    Solution1 s1;
+    cout << s1.kthCharacter(5, operations) << endl;
     return 0;
 }
